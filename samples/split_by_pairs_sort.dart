2bd5001232749728000177e99f2c2a0832a26722
Vladimir Rybakov <vladimir.rybakov@gmail.com>
2024-11-18 21:54:57 +0100

    the second fix for players distribution

--- samples/split_by_pairs_sort.dart
+++ samples/split_by_pairs_sort.dart
@@ -185,29 +185,38 @@ void main(List<String> args) {
       
       var round = schedule[r];
       print (schedule[r]);
-      while(round.isNotEmpty) {
-        var pair = round[0];
-        round.removeAt(0);
 
-        if (round.isEmpty) {
-          sortedRound.add(pair);
+      while(round.isNotEmpty) {
+        if (round.length == 1) {
+          sortedRound.add(round[0]);
           break;
         }
 
-        int enemyIdx = 0;
-        int enemyMin = players;
-        for (int j = 0; j < round.length; j += 1) {
-          var enemyPair = round[j];
-          int enemyCnt = 0;
-          enemyCnt += matrix[pair[0] - 1][enemyPair[0] - 1] + matrix[pair[0] - 1][enemyPair[1] - 1];
-          enemyCnt += matrix[pair[1] - 1][enemyPair[0] - 1] + matrix[pair[1] - 1][enemyPair[1] - 1];
-          if (enemyCnt < enemyMin) {
-            enemyIdx = j;
+        int fstIdx = 0;
+        int sndIdx = 1;
+        int minWeight = players;
+        for (int i = 0; i < round.length - 1; i += 1) {
+          for (int j = i + 1; j < round.length; j += 1) {
+            var fstPair = round[i];
+            var sndPair = round[j];
+
+            int weight = 0;
+            weight += matrix[fstPair[0] - 1][sndPair[0] - 1] + matrix[fstPair[0] - 1][sndPair[1] - 1];
+            weight += matrix[fstPair[1] - 1][sndPair[0] - 1] + matrix[fstPair[1] - 1][sndPair[1] - 1];
+
+            if (weight < minWeight) {
+              minWeight = weight;
+              fstIdx = i;
+              sndIdx = j;
+            }
           }
         }
-        sortedRound.add(pair);
-        sortedRound.add(round[enemyIdx]);
-        round.removeAt(enemyIdx);
+
+        sortedRound.add(round[fstIdx]);
+        sortedRound.add(round[sndIdx]);
+
+        round.removeAt(sndIdx);
+        round.removeAt(fstIdx);
       }
 
       schedule[r] = sortedRound;
@@ -226,8 +235,13 @@ void main(List<String> args) {
         matrix[sndTeam[1] - 1][fstTeam[0] - 1] += 1;
         matrix[sndTeam[1] - 1][fstTeam[1] - 1] += 1;
       }
-
       print (schedule[r]);
+
+      // Print the matrix
+      for (var row in matrix) {
+        print(row);
+      }      
+      print('\n\n');
     }
 
     // Print the matrix
@@ -255,5 +269,35 @@ void main(List<String> args) {
 [3, 2, 0, 2, 2, 3, 0, 3, 3, 1, 1, 3, 3, 0, 2]
 [1, 1, 1, 0, 4, 7, 1, 1, 2, 1, 2, 1, 4, 2, 0]
 
+[0, 1, 2, 3, 1, 0, 1, 1, 4, 4, 3, 3, 1, 4, 0]
+[1, 0, 3, 2, 2, 2, 1, 1, 3, 0, 3, 2, 1, 4, 1]
+[2, 3, 0, 3, 5, 2, 1, 3, 2, 2, 0, 2, 1, 0, 2]
+[3, 2, 3, 0, 4, 1, 1, 2, 3, 2, 0, 0, 3, 3, 1]
+[1, 2, 5, 4, 0, 2, 1, 2, 1, 2, 1, 1, 2, 1, 3]
+[0, 2, 2, 1, 2, 0, 4, 1, 3, 1, 4, 1, 2, 0, 5]
+[1, 1, 1, 1, 1, 4, 0, 3, 5, 2, 2, 3, 2, 1, 1]
+[1, 1, 3, 2, 2, 1, 3, 0, 0, 2, 2, 2, 4, 2, 3]
+[4, 3, 2, 3, 1, 3, 5, 0, 0, 3, 0, 1, 1, 1, 1]
+[4, 0, 2, 2, 2, 1, 2, 2, 3, 0, 3, 1, 1, 0, 5]
+[3, 3, 0, 0, 1, 4, 2, 2, 0, 3, 0, 3, 0, 3, 4]
+[3, 2, 2, 0, 1, 1, 3, 2, 1, 1, 3, 0, 4, 5, 0]
+[1, 1, 1, 3, 2, 2, 2, 4, 1, 1, 0, 4, 0, 3, 1]
+[4, 4, 0, 3, 1, 0, 1, 2, 1, 0, 3, 5, 3, 0, 1]
+[0, 1, 2, 1, 3, 5, 1, 3, 1, 5, 4, 0, 1, 1, 0]
 
+[0, 2, 1, 1, 2, 3, 2, 1, 2, 1, 5, 3, 1, 1, 3]
+[2, 0, 2, 2, 2, 2, 2, 2, 3, 2, 0, 3, 2, 2, 2]
+[1, 2, 0, 2, 1, 3, 3, 4, 1, 3, 1, 2, 1, 2, 0]
+[1, 2, 2, 0, 3, 3, 2, 2, 1, 3, 1, 2, 3, 1, 2]
+[2, 2, 1, 3, 0, 2, 2, 1, 3, 3, 2, 1, 1, 3, 2]
+[3, 2, 3, 3, 2, 0, 2, 1, 1, 1, 0, 2, 2, 1, 3]
+[2, 2, 3, 2, 2, 2, 0, 3, 1, 1, 2, 2, 2, 2, 2]
+[1, 2, 4, 2, 1, 1, 3, 0, 2, 2, 1, 2, 1, 4, 2]
+[2, 3, 1, 1, 3, 1, 1, 2, 0, 3, 2, 2, 3, 2, 2]
+[1, 2, 3, 3, 3, 1, 1, 2, 3, 0, 3, 2, 1, 1, 2]
+[5, 0, 1, 1, 2, 0, 2, 1, 2, 3, 0, 2, 4, 4, 1]
+[3, 3, 2, 2, 1, 2, 2, 2, 2, 2, 2, 0, 2, 1, 2]
+[1, 2, 1, 3, 1, 2, 2, 1, 3, 1, 4, 2, 0, 2, 3]
+[1, 2, 2, 1, 3, 1, 2, 4, 2, 1, 4, 1, 2, 0, 2]
+[3, 2, 0, 2, 2, 3, 2, 2, 2, 2, 1, 2, 3, 2, 0]
 */
